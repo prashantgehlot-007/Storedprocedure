dbfe96c980a0e7ece26359b65fc762ab824901c7
Prashant Gehlot <findingprashant007@gmail.com>
2020-01-14 01:04:58 +0530

    Add project files.

---
+++ Storedprocedure/SpDemo.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SpDemo.aspx.cs" Inherits="Storedprocedure.SpDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="background-color:peachpuff">
+    <form id="form1" runat="server">
+        <div style="color: #FF0000; font-weight: bold; font-style: italic; font-size: large">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Books Details<br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Book Id&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Book Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Price&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" Font-Bold="True" Font-Italic="True" ForeColor="#CC0000" Text="Save" OnClick="Button1_Click" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" Font-Bold="True" Font-Italic="True" ForeColor="#CC0000" Text="Search" OnClick="Button2_Click" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button3" runat="server" Font-Bold="True" Font-Italic="True" ForeColor="#CC0000" Text="Update" OnClick="Button3_Click" />
+&nbsp;&nbsp;
+            <asp:Button ID="Button4" runat="server" Font-Bold="True" Font-Italic="True" ForeColor="#CC0000" Text="Delete" OnClick="Button4_Click" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button5" runat="server" Font-Bold="True" Font-Italic="True" ForeColor="#CC0000" Text="ShowAll" OnClick="Button5_Click" />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;
+            <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" style="margin-left: 145px" Width="482px">
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#7C6F57" />
+                <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#E3EAEB" />
+                <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                <SortedAscendingHeaderStyle BackColor="#246B61" />
+                <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                <SortedDescendingHeaderStyle BackColor="#15524A" />
+            </asp:GridView>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Storedprocedure/SpDemo.aspx.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Storedprocedure
+{
+    public partial class SpDemo : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        SqlDataAdapter da;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=H:\Storedprocedure\Storedprocedure\App_Data\storepdemo.mdf;Integrated Security=True");
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //save button
+            try
+            {
+                cmd = new SqlCommand("SaveBook", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@BId", TextBox1.Text);
+                cmd.Parameters.AddWithValue("@BName", TextBox2.Text);
+                cmd.Parameters.AddWithValue("@Price", TextBox3.Text);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                TextBox1.Text = " ";
+                TextBox2.Text = " ";
+                TextBox3.Text = " ";
+                Response.Write("Date Saved");
+            }
+            catch(Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //Search Butoon
+            try
+            {
+                cmd = new SqlCommand("SearchBook", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@BId", TextBox1.Text);
+                conn.Open();
+                dr = cmd.ExecuteReader();
+                if(dr.Read())
+                {
+                    TextBox1.Text = dr["BId"].ToString();
+                    TextBox2.Text = dr["BName"].ToString();
+                    TextBox3.Text = dr["Price"].ToString();
+
+                }
+                else
+                {
+                    Response.Write("Data Not Found");
+                }
+            }
+            catch(Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            //update button
+            try
+            {
+                cmd = new SqlCommand("UpdateBook", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@BId", TextBox1.Text);
+                cmd.Parameters.AddWithValue("@BName",TextBox2.Text);
+                cmd.Parameters.AddWithValue("@Price", TextBox3.Text);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                TextBox1.Text = " ";
+                TextBox2.Text = " ";
+                TextBox3.Text = " ";
+                Response.Write("Data Saved");
+            }
+            catch(Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            //Delte button
+            try
+            {
+                cmd = new SqlCommand("DeleteBook", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@BId", TextBox1.Text);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                TextBox1.Text = " ";
+                TextBox2.Text = " ";
+                TextBox3.Text = " ";
+                Response.Write("Data Deleted");
+            }
+            catch(Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            //Showall
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("ShowAllBooks", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            catch(Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+        }
+    }
+}
